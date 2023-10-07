75ce62d8f4400f29d5d8cc5e03664536279ce2e0
azmat <azmatyusuf2022@gmail.com>
2023-10-07 14:30:00 +0530

    added get_data_for_ticker() to get companies data for a time period

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 import pickle
-
+import os
+import datetime as dt
+import yfinance as yf
 
 def grab_tickers(reload=False):
     # if reload is True then we will refresh the list of tickers
@@ -22,3 +24,26 @@ def grab_tickers(reload=False):
         tickers = pickle.load(f)
         # print(tickers)
         return tickers
+
+def get_data_for_ticker(reload_data=False, reload_tickers=False):
+    tickers = grab_tickers(reload=reload_tickers)
+
+    if not os.path.exists('stocks_dfs'):
+        os.makedirs('stocks_dfs')
+
+    start = dt.datetime(2017, 1, 1)
+    end = dt.datetime(2022, 12, 31)
+
+    for ticker in tickers:
+        ticker = ticker.rstrip()
+        if reload_data:
+           if not os.path.exists(f'stocks_dfs/{ticker}.csv'):
+               df = yf.download(ticker, start, end)
+               df.to_csv(f'stocks_dfs/{ticker}.csv')
+           else:
+               print(f'Already have {ticker}.csv')
+        else:
+            pass
+
+
+# get_data_for_ticker(reload_data=True)
